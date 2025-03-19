133b47f2b7a228bf576907be92159bb7461bf2fa
harabagiuandrei <harabagiuandreipaul@yahoo.com>
2025-03-19 20:35:56 +0200

    Add files via upload

---
+++ lobe_link_neurons_LLN.py
@@ -0,0 +1,8 @@
+#daca functioneaza ca si neuroni de legatura intre retelele neuronale
+#au o stare duala!! :)))
+#exact ca in entanglement
+#e ca si cum am avea un IN/OUT intre 2 retele neuronale cu neuron de legatura in fiecare
+#daca reteaua A activeaza reteaua B prin nodul de legatura qtica Q1,
+#asta va modifica orice urmator raspuns de analiza efectuat de reteaua neuronala B
+#sau poate.. (caz 2 de efect) reteaua B , oricum prin reconfigurarea/adaptarea continua
+#ce se intampla in mod normal, elimina efectul de pe nodul bQ1 (vom numi nodul de legatura qtica din reteaua A aQ1)
